d812ecf8228b73ecd16077706bc2f987f09d91f1
Francis J <fajoaq@gmail.com>
2021-05-20 20:25:16 -0400

    Created container with safeareaview

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  FlatList, 
+  SafeAreaView, 
+  Platform, 
+  TouchableOpacity, 
+  StatusBar 
+} from 'react-native';
 
-export default function App() {
+const IS_IOS = Platform.OS === 'ios';
+const DATA = [
+  {
+    id: 1,
+    title: "Some data"
+  },
+  {
+    id: 2,
+    title: "Some data"
+  }
+];
+
+const App = () => {
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity>
+        <Text>{ item.title }</Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <React.Fragment>
+      <SafeAreaView style={styles.container}>
+        <Text style={ styles.search }>Page content</Text>
+        <FlatList 
+          data={ DATA }
+          renderItem={ renderItem }
+          keyExtractor={ item => item.id.toString() }
+          style={ styles.list}
+        />
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </React.Fragment>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: IS_IOS ? 0 : StatusBar.currentHeight,
   },
+  search: {
+    paddingVertical: 10,
+    padding: 16,
+    backgroundColor: 'green'
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue'
+  }
 });
+
+export default App;
