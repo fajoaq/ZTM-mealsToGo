2940e9be8dea37298c5231aed0ccfe44d69d0853
Francis J <fajoaq@gmail.com>
2021-06-05 00:06:17 -0400

    added open and closed components to info card

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Avatar, Button, Card, Paragraph } from 'react-native-paper';
+import { Image, Text } from 'react-native';
+import { Card } from 'react-native-paper';
 import {SvgXml } from 'react-native-svg';
 import styled from 'styled-components/native';
 
 import star from '../../../../assets/star';
+import open from '../../../../assets/open';
+import { space } from '../../../infrastructure/theme/spacing'
 
 const RestaurantCard = styled(Card)`
     background-color: ${props => props.theme.colors.bg.primary};
@@ -27,9 +30,23 @@ const Info = styled.View`
 
 const Rating = styled.View`
     flex-direction: row
+    align-items: center
     padding-top: ${(props) => props.theme.space[2]}
     padding-bottom: ${(props) => props.theme.space[2]}
 `;
+
+const Section = styled.View`
+    flex-direction: row
+    align-items: center
+`;
+
+const SectionEnd = styled.View`
+    flex: 1
+    flex-direction: row
+    justify-content: flex-end
+`;
+
+const Open = styled(SvgXml)``;
 
 const Address = styled.Text`
     font-family: ${(props) => props.theme.fonts.body}
@@ -39,14 +56,14 @@ const Address = styled.Text`
 export const RestaurantInfoCard = ({ restaurant={} }) => {
     const {
         name="Some Restaurant",
-        icon,
+        icon="https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
         photos=[
             "https://www.foodiesfeed.com/wp-content/uploads/2020/09/breakfast-sandwich-with-hummus-spread-and-fresh-vegetables-1.jpg"
         ],
         address="Some random street",
         rating=4,
-        openNow=true,
-        isClosedTemporarily
+        isOpenNow=true,
+        isClosedTemporarily=false
     } = restaurant;
 
     const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -56,10 +73,21 @@ export const RestaurantInfoCard = ({ restaurant={} }) => {
             <RestaurantCardCover source={{ uri: photos[0] }} />
             <Info>
                 <Title>{ name }</Title>
-                <Rating>
+                <Section>
+                    <Rating>
                     { ratingArray.map((_,index) => (<SvgXml key={index} xml={ star } width={ 20 } height={ 20 } />
-                    ))}
-                </Rating>
+                        ))}
+                    </Rating>
+                    <SectionEnd>
+                        { isClosedTemporarily && 
+                            <Text variant="label" style={{ marginLeft: 11, color: 'red' }}>Closed Temporarily</Text> 
+                        }
+                        { isOpenNow && 
+                            <SvgXml style={{ marginLeft: 11}} xml={ open } width={ 20 } height={ 20 } /> 
+                        }
+                        <Image style={{ marginLeft: 11, width: 15, height: 15}}  source={{uri: icon}} />
+                    </SectionEnd>
+                </Section>
                 <Address>{ address }</Address>
             </Info>
         </RestaurantCard>
