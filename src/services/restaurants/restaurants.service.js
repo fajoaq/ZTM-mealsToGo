014cdbdf7a5a35aaf0bd50b8b76faabd936589b0
Francis J <fajoaq@gmail.com>
2021-06-21 20:55:23 -0400

    created restaurants service request function

---
+++ src/services/restaurants/restaurants.service.js
@@ -0,0 +1,22 @@
+import { mocks } from "./mock";
+import camelize from 'camelize';
+
+export const restaurantsRequest = async (location = "37.7749295,-122.4194155") => {
+    const mock = await mocks[location];
+
+    if(!mock) return new Error("Not found");
+
+    return mock;
+};
+
+const restaurantsTransform = ({ results=[] }) => {
+    const mappedResults = results.map((restaurant) => {
+        return {
+            ...restaurant,
+            isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+            isClosedTemporarily: restaurant.business_status === 'CLOSED_TEMPORARILY'
+        }
+    });
+
+    return camelize(mappedResults);
+};
