e649780b78b5f517f49bc519e80c53eed5092979
Francis J <fajoaq@gmail.com>
2021-07-08 17:24:18 -0400

    linked map pins to restaurant details screen

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -12,7 +12,7 @@ const Map = styled(MapView)`
     width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
     const { location } = useContext(LocationContext);
     const { lat, lng, viewport } = location;
     const { restaurants = [] } = useContext(RestaurantsContext);
@@ -44,7 +44,9 @@ export const MapScreen = () => {
                             longitude: restaurant.geometry.location.lng,
                         }}
                     >
-                        <MapView.Callout>
+                        <MapView.Callout
+                            onPress={() => navigation.navigate("RestaurantDetail",  { restaurant })}
+                        >
                            <MapCallout restaurant={ restaurant } />
                         </MapView.Callout>
                     </MapView.Marker>
